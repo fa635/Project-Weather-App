4f9356640697473fcd6b6565a3d9789250f73e2d
fa635 <bfalonne823@gmail.com>
2024-02-10 05:19:12 +0100

    Add user input

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-let location = "london"
+let location
+location = prompt("enter city name")
 
 async function getWeather () {
     const response = await fetch("http://api.weatherapi.com/v1/current.json?key=bc6e967a6fbc485281343850240802&q=" + location + "&aqi=no", {mode: 'cors'})
