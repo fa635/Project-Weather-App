d6f211c6730d9ca3a5f872dd6a55bcf8975d531b
fa635 <bfalonne823@gmail.com>
2024-02-10 07:56:04 +0100

    Add gifs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 let location
 location = prompt("enter city name")
 
+
+
 async function getWeather () {
     const response = await fetch("http://api.weatherapi.com/v1/current.json?key=bc6e967a6fbc485281343850240802&q=" + location + "&aqi=no", {mode: 'cors'})
     const weatherData = await response.json()
@@ -15,6 +17,22 @@ async function getWeather () {
     body.appendChild(div)
 
     div.textContent = "temp_c: " + weatherData.current.feelslike_c
+
+
+    async function getGifs () {
+        const res = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=qIHPBWfAYAqkZZPhbzBfPxrV553QTJV1&s=cats', {mode: 'cors'})
+        const gifsData = await res.json()
+
+        console.log(gifsData.data.images.original.url)
+
+        
+
+
+        const body = document.querySelector("body")
+        body.setAttribute("style", "background-image: url(" + gifsData.data.images.original.url + ")")
+        
+    }
+    getGifs()
     
 
 }
