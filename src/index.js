282f25798c523dd2b5234d6da84d67e5c80a6bd7
fa635 <bfalonne823@gmail.com>
2024-02-10 07:05:21 +0100

    Display data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+
+
+
 let location
 location = prompt("enter city name")
 
@@ -5,12 +8,16 @@ async function getWeather () {
     const response = await fetch("http://api.weatherapi.com/v1/current.json?key=bc6e967a6fbc485281343850240802&q=" + location + "&aqi=no", {mode: 'cors'})
     const weatherData = await response.json()
     console.log("temp_c: " + weatherData.current.feelslike_c)
+
+    const body = document.querySelector("body")
+    const div = document.createElement("div")
+
+    body.appendChild(div)
+
+    div.textContent = "temp_c: " + weatherData.current.feelslike_c
+    
+
 }
 getWeather()
-
-
-
-
-
 
 
